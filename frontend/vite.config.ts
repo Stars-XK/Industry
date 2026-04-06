714932b6979e1a3966c150ba556a50d9f838b1f5
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-06 12:33:08 +0000

    fix(frontend): 添加 vite 本地开发代理规则，解决跨域及 404 问题

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,17 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000,
+    port: 5173, // 前端默认开发端口
     open: true,
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:3001', // auth-service 所在的端口
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://localhost:3001', // 后端微服务或网关所在的端口
+        changeOrigin: true,
+      }
+    }
   },
 });

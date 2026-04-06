47ee4789cdb08c03264e64bdb6c1ec6739eed227
Stars-XK <Stars-XK@users.noreply.github.com>
2026-04-06 10:59:10 +0000

    feat: 补充前后端框架的底层脚手架代码(main.ts/App.vue/app.module.ts)与package依赖，使其具备真实的编译运行能力

---
+++ frontend/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+    <h1>信创工业综合治理平台 - 前端工程初始化成功</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // 基础的 Vite 配置
 export default defineConfig({
+  plugins: [vue()],
   server: {
     port: 3000,
     open: true,
